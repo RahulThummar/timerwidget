83b112e5dea58c71583fd123ec6c1e3afc70e2d9
Rahul Thummar <rthummar788@rku.ac.in>
2024-01-10 10:33:34 +0530

    changes traffic light hight

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <template>
   <AllTypesWidget />
-  <!-- <TimerWidget />
-  <TrafficLightWidget /> -->
 </template>
 
 <script>
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/style.css";
-// import TimerWidget from "./components/TimerWidget.vue";
-// import TrafficLightWidget from "./components/TrafficLightWidget.vue";
 import AllTypesWidget from "./components/AllTypesWidget.vue";
 
 export default {
   name: "App",
   components: {
-    // TimerWidget,
-    // TrafficLightWidget,
     AllTypesWidget,
   },
 };
