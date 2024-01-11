75678bd7f055c099e76116567a9e44f21c3e5ba7
Rahul Thummar <rthummar788@rku.ac.in>
2024-01-11 18:04:32 +0530

    changes

--- src/components/SpinnerWidget.vue
+++ src/components/SpinnerWidget.vue
@@ -8,16 +8,17 @@
     }"
   >
     <div>
-      <div class="spinner-main">
+      <div v-if="this.isItemAdded" class="filled-spinner-main">
         <div class="arrow-img">
-          <img :src="arrow" :style="{ width: '40px', height: '40px' }" />
+          <img :src="arrow" :style="{ width: '55px', height: '40px' }" />
         </div>
-        <div class="start-spin-img ">
+        <div class="start-spin-img">
           <div v-if="!this.spinnerStart" class="play-spin-img">
             <img :src="play_icn" :style="{ width: '20px', height: '20px' }" />
           </div>
           <img :src="play_bg" :style="{ width: '60px', height: '60px' }" />
         </div>
+
         <FortuneWheel
           style="width: 300px"
           borderColor="#00000033"
@@ -28,10 +29,31 @@
           @rotateEnd="onRotateEnd"
         />
       </div>
+      <div v-else class="empty-spinner-main">
+        <div class="empty-circle">
+          <div class="col-9">
+            <button
+              class="btn-common save"
+              @click="
+                () => {
+                  showAddModal = true;
+                }
+              "
+            >
+              Create New Spinner
+            </button>
+          </div>
+          <div class="col-9">
+            <button class="btn-common save mt-5" @click="handleSaveSound()">
+              Choose Spinner
+            </button>
+          </div>
+        </div>
+      </div>
 
       <!-- bottom buttons -->
 
-      <!-- <div
+      <div
         :class="{
           'bottom-buttons': !rotation,
           'bottom-buttons-1': rotation,
@@ -40,7 +62,7 @@
       >
         <div>
           <img
-            :src="RottetImg"
+            :src="editImg"
             class="pointer"
             alt="reset timer"
             :style="{ width: '36px', height: '36px' }"
@@ -65,10 +87,52 @@
             @click="startSound"
           />
         </div>
-      </div> -->
+      </div>
     </div>
   </div>
-  <audio ref="tickAudio" :src="ClickSound"></audio>
+
+  <audio ref="tickAudio" :src="celebration_bell"></audio>
+
+  <div class="form-main mt-5" v-if="this.showAddModal">
+    <span class="form-inner">
+      <div class="name-label">Tag Name</div>
+      <span class="input-main">
+        <input v-model="message" placeholder="Enter Tag Name" />
+        <div class="enter-btn-main" @click="handleClickTagEnter">
+          <img loading="lazy" :src="EnterImg" class="enter-img pointer" />
+        </div>
+      </span>
+
+      <div class="tag-main">
+        <span
+          class="tag-inner"
+          v-for="(tagNames, index) in tagList"
+          :key="index"
+        >
+          {{ tagNames.name }}
+          <img
+            loading="lazy"
+            class="pointer"
+            :src="cancelImg"
+            @click="handleClickTagRemove(index)"
+          />
+        </span>
+      </div>
+
+      <div class="cancle-save-btn">
+        <span
+          class="btn-common cancle col-6 me-2 pointer"
+          @click="handleClickCancle"
+          >Cancel</span
+        >
+        <span
+          class="btn-common save col-6 pointer"
+          @click="handleClickSaveItems"
+          >Save</span
+        >
+      </div>
+    </span>
+  </div>
 </template>
 
 <script>
@@ -78,17 +142,22 @@ import lottie from "lottie-web";
 import animationData from "../assets/images/celebration.json";
 import unFilledtimerImg from "@/assets/images/TrafficLightWidget/unFilledtimerImg.png";
 import filledTrafficlightImg from "@/assets/images/TrafficLightWidget/filledTrafficlight.png";
-import RottetImg from "@/assets/images/TrafficLightWidget/RottetImg.png";
 import VolumOn from "@/assets/images/TimerWidget/VolumOn.png";
 import VolumOff from "@/assets/images/TimerWidget/VolumOff.png";
 import Sun from "@/assets/images/TimerWidget/Sun.png";
 import Moon from "@/assets/images/TimerWidget/Moon.png";
 import arrow from "@/assets/images/SpinnerWidget/arrow.png";
+import editImg from "@/assets/images/SpinnerWidget/editImg.png";
 import play_icn from "@/assets/images/SpinnerWidget/play_icn.png";
+import Rectangle from "@/assets/images/SpinnerWidget/Rectangle.png";
+import EnterImg from "@/assets/images/SpinnerWidget/EnterImg.png";
+import cancelImg from "@/assets/images/SpinnerWidget/cancelImg.png";
 import play_bg from "@/assets/images/SpinnerWidget/play_bg.png";
-import ClickSound from "@/assets/sound/TrafficLightWidget/ClickSound.mp3";
+import celebration_bell from "@/assets/sound/SpinnerWidget/celebration_bell.mp3";
 import { colorsList } from "./constants/ConstData.js";
+import { ref } from "vue";
 
+let message = ref("");
 export default {
   components: {
     FortuneWheel,
@@ -101,93 +170,14 @@ export default {
     }
   },
   data() {
+    console.log(this.tagText);
     return {
+      tagList: [],
+      message: message,
       canvasVerify: true,
-      prizes: [
-        {
-          id: 1,
-          name: "1",
-          value: "1",
-          bgColor: "#45ace9",
-          color: "#ffffff",
-          probability: 10,
-        },
-        {
-          id: 2,
-          name: "2",
-          value: "2",
-          bgColor: "#dd3832",
-          color: "#ffffff",
-          probability: 10,
-        },
-        {
-          id: 3,
-          name: "3",
-          value: "3",
-          bgColor: "#fef151",
-          color: "#ffffff",
-          probability: 10,
-        },
-        {
-          id: 4,
-          name: "4",
-          value: "4",
-          bgColor: "pink",
-          color: "blue",
-          probability: 10,
-        },
-        {
-          id: 5,
-          name: "5",
-          value: "5",
-          bgColor: "#32a852",
-          color: "blue",
-          probability: 10,
-        },
-        {
-          id: 6,
-          name: "6",
-          value: "6",
-          bgColor: "#30898c",
-          color: "blue",
-          probability: 10,
-        },
-        {
-          id: 7,
-          name: "7",
-          value: "7",
-          bgColor: "#05e6ed",
-          color: "blue",
-          probability: 10,
-        },
-        {
-          id: 8,
-          name: "8",
-          value: "8",
-          bgColor: "#dbb753",
-          color: "blue",
-          probability: 10,
-        },
-        {
-          id: 9,
-          name: "9",
-          value: "9",
-          bgColor: "#c9c5de",
-          color: "blue",
-          probability: 10,
-        },
-        {
-          id: 10,
-          name: "10",
-          value: "10",
-          bgColor: "#8348b0",
-          color: "blue",
-          probability: 10,
-        },
-      ],
-
-      ClickSound: ClickSound,
-      RottetImg: RottetImg,
+      prizes: [],
+      celebration_bell: celebration_bell,
+      editImg: editImg,
       unFilledtimerImg: unFilledtimerImg,
       filledTrafficlightImg: filledTrafficlightImg,
       selectedSound: null,
@@ -195,6 +185,9 @@ export default {
       Moon: Moon,
       play_bg: play_bg,
       arrow: arrow,
+      EnterImg: EnterImg,
+      Rectangle: Rectangle,
+      cancelImg: cancelImg,
       play_icn: play_icn,
       VolumOn: VolumOn,
       VolumOff: VolumOff,
@@ -202,6 +195,8 @@ export default {
       IsLightMode: true,
       colorTypes: colorsList,
       spinnerStart: false,
+      isItemAdded: false,
+      showAddModal: false,
     };
   },
 
@@ -211,11 +206,75 @@ export default {
     },
   },
   methods: {
-    handleStartSpinner() {
-      console.log("*************");
-      //   this.spinnerStart = true;
-      //   document.getElementsByClassName("fw-btn").click();
+    handleClickTagEnter() {
+      let randomColor;
+      if (message.value.trim() !== "") {
+        for (let i = 0; i < 10; i++) {
+          const color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+          randomColor = color;
+        }
+        this.tagList.push({
+          id: this.tagList.length + 1,
+          name: message.value.trim(),
+          bgColor: randomColor,
+          color: "#fff",
+          probability: 0,
+        });
+        message.value = "";
+      }
+      console.log(this.tagList);
     },
+
+    handleClickSaveItems() {
+      console.log(this.tagList.length, "length");
+      if (this.tagList.length < 2) {
+        alert("Please add two or more items.");
+        return;
+      }
+
+      let probabilityRatio = 100 / this.tagList.length;
+      console.log(probabilityRatio);
+      this.tagList.map((a) => (a.probability = probabilityRatio));
+      this.prizes = this.tagList;
+
+      let arr = this.prizes;
+
+      if (
+        this.prizes.reduce((n, { probability }) => n + probability, 0) !== 100
+      ) {
+        let temp =
+          100 - this.prizes.reduce((n, { probability }) => n + probability, 0);
+        arr[0].probability = arr[0].probability + temp;
+      }
+      this.prizes = arr;
+
+      if (
+        this.prizes.reduce((n, { probability }) => n + probability, 0) === 100
+      ) {
+        console.log(this.prizes.length, "5555555588888");
+        console.log(this.prizes, "55555555");
+        this.showAddModal = false;
+        this.isItemAdded = true;
+      }
+      setTimeout(() => {
+        var element = document.getElementsByClassName("fw-btn__btn")[0];
+        console.log(element);
+        if (element) {
+          element.classList.add("play-spinner-btn");
+        }
+      }, 1);
+    },
+
+    handleClickTagRemove(index) {
+      this.tagList.splice(index, 1);
+    },
+
+    handleClickCancle() {
+      this.tagList = [];
+      this.prizes = [];
+      this.showAddModal = false;
+    },
+
     onImageRotateStart() {
       console.log("onRotateStart");
     },
@@ -240,8 +299,11 @@ export default {
       console.log("onCanvasRotateStart");
     },
     onRotateEnd(prize) {
+      if (this.IsSoundOn) {
+        this.$refs.tickAudio.play();
+      }
       this.spinnerStart = false;
-      alert(prize.value);
+      alert(prize.name);
     },
 
     DoServiceVerify(verified, duration) {
@@ -286,3 +348,10 @@ export default {
 @import "../assets/style.css";
 @import "~bootstrap-icons/font/bootstrap-icons.css";
 </style>
+
+<!-- id: 1,
+name: "1",
+value: "1",
+bgColor: "#45ace9",
+color: "#ffffff",
+probability: 10, -->
